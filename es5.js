9e8b2746d8e600f49d5be298460eeb5264406d62
sebastien-p <sp@fing.pro>
2016-02-03 14:33:06 +0100

    Use plain javascript instead of yaml

--- es5.js
+++ es5.js
@@ -1,3 +1,16 @@
 'use strict';
 
-module.exports = require('yamljs').load('.eslintrc.es5.yaml');
+module.exports = {
+  root: true,
+  extends: [
+    './lib/es5/env.js',
+    './lib/es5/ecma-features.js',
+    './lib/es5/possible-errors.js',
+    './lib/es5/best-practices.js',
+    './lib/es5/strict-mode.js',
+    './lib/es5/variables.js',
+    './lib/es5/nodejs-and-commonjs.js',
+    './lib/es5/stylistic-issues.js',
+    './lib/es5/ecmascript-6.js'
+  ]
+};
--- es6.js
+++ es6.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('yamljs').load('.eslintrc.es6.yaml');
+module.exports = {
+  root: true,
+  extends: [
+    './es5.js',
+    './lib/es6/env.js',
+    './lib/es6/ecma-features.js',
+    './lib/es6/ecmascript-6.js'
+  ]
+};
