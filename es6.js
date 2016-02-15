5251ec0f1b3b8a52683b2d490a473323b3a4f71f
sebastien-p <sp@fing.pro>
2016-02-15 16:38:58 +0100

    Disable no-magic-numbers rule and use parser-options.js files

--- es6.js
+++ es6.js
@@ -4,8 +4,8 @@ module.exports = {
   root: true,
   extends: [
     './es5',
+    './lib/es6/parser-options',
     './lib/es6/env',
-    './lib/es6/ecma-features',
     './lib/es6/ecmascript-6'
   ].map(require.resolve)
 };
